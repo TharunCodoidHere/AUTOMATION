1e9dcbaa6c49a2ad5047b22509d7e672c5426c25
TharunCodoidHere <tharunkumar.ka@codoid.com>
2022-10-04 13:34:41 +0530

    update process

--- src/Demo/New.java
+++ src/Demo/New.java
@@ -1,6 +1,16 @@
-package Demo;
+class Main {
+    public static void main(String[] args) {
 
-import Sample.PrimeNumber;
+        int n = 10, firstTerm = 0, secondTerm = 1;
+        System.out.println("Fibonacci Series till " + n + " terms:");
 
-public class New extends PrimeNumber {
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(firstTerm + ", ");
+
+            // compute the next term
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+    }
 }
--- src/Sample/Fibbonocci.java
+++ src/Sample/Fibbonocci.java
@@ -1,7 +1,7 @@
 package Sample;
 
 public class Fibbonocci {
-    public static void main(String[] args) {
+     public void main(String[] args) {
         int n1=0, n2=1,n3,i,count=10;
         System.out.println(n1+" " +n2);
         for (i=2;i<count;i++){
